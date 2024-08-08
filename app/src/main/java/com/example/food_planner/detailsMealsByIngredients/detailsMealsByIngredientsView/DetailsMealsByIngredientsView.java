10556e3d56a036b354fb4d5e1812cee2eb491efe
Ahmed Samy <ahmbdelkhalek@gmail.com>
2024-08-08 21:15:39 +0300

    made onSuccess and onFailure methods

--- app/src/main/java/com/example/food_planner/detailsMealsByIngredients/detailsMealsByIngredientsView/DetailsMealsByIngredientsView.java
+++ app/src/main/java/com/example/food_planner/detailsMealsByIngredients/detailsMealsByIngredientsView/DetailsMealsByIngredientsView.java
@@ -1,4 +1,8 @@
 package com.example.food_planner.detailsMealsByIngredients.detailsMealsByIngredientsView;
 
+import com.example.food_planner.model.dto_repos.ResponseMealByIngredientDto;
+
 public interface DetailsMealsByIngredientsView {
+    void onAllMealsByIngredientsSuccess(ResponseMealByIngredientDto responseMealByIngredientDto);
+    void onAllMealsByIngredientsFailure(String errMessage);
 }
