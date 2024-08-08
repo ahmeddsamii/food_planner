e0285ec6218b7dffb562e62dcd063fc692d03a9b
Ahmed Samy <ahmbdelkhalek@gmail.com>
2024-08-08 20:35:39 +0300

    added two functions which are success of mealsByIngredient and failure

--- app/src/main/java/com/example/food_planner/Repo/NetworkCallBack.java
+++ app/src/main/java/com/example/food_planner/Repo/NetworkCallBack.java
@@ -4,6 +4,7 @@ import com.example.food_planner.model.dto_repos.ResponseAllIngredients;
 import com.example.food_planner.model.dto_repos.ResponseCategory;
 import com.example.food_planner.model.dto_repos.ResponseCountry;
 import com.example.food_planner.model.dto_repos.ResponseIngredient;
+import com.example.food_planner.model.dto_repos.ResponseMealByIngredientDto;
 import com.example.food_planner.model.dto_repos.ResponseMealInfoDto;
 import com.example.food_planner.model.dto_repos.ResponseMeals;
 import com.example.food_planner.model.dtos.MealDto;
@@ -37,4 +38,8 @@ public interface NetworkCallBack {
 
     void onAllIngredientSuccess(ResponseAllIngredients allIngredients);
     void onAllIngredientsFailure(String errMessage);
+
+
+    void onAllMealsByIngredientsSuccess(ResponseMealByIngredientDto meals);
+    void onAllMealsByIngredientsFailure(String errMessage);
 }
